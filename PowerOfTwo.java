65fe69bff3e5b1152b0dd3d4cdbc9593146f5a7a
vikas <kvxcoolsma@gmail.com>
2025-09-22 17:26:29 +0530

    update/fix-error leetcode problem-231

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -1,19 +1,13 @@
 // Leetcode 231: Power of Two
 
-
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==1)return true;
-        if(n > Integer.MAX_VALUE) return true;
-        for(int i=1;i<n;i++){
-            int a = ((int)Math.pow(2,i));
-            if(a == n){
-                return true;
-            }
-            if(a > n){
-                return false;
-            }
+        if(n<=0) return false;
+        if(n==1) return true;
+        while(n>1){
+            if(n%2!=0) return false;
+            n/=2;
         }
-        return false;
-         }
+        return true;
+    }
 };
